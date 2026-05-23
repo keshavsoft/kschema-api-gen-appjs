8e77552940a068e0ff43dfc10430f1c458c47e8f
keshavsoft <nknnkumar@yahoo.com>
2026-05-23 17:33:20 +0530

    test

--- index.js
+++ index.js
@@ -3,27 +3,7 @@ import getLatestVersion from "./bin/core/getLatestVersion.js";
 const load = async () => {
     const v = getLatestVersion();
 
-    return import(`../bin/${v}/core/resolveCommand.js`);
+    return await import(`../bin/${v}/StartEndPoint/index.js`);
 };
 
-const startFunc = async ({ inCommandToSend, inFolderName,
-    inToPath
-}) => {
-    const { default: run } = await load();
-
-    const fn = run(inCommandToSend);
-
-    fn({
-        folderName: inFolderName,
-        toPath: inToPath,
-        showLog: false,
-        isAnnounce: false
-    });
-};
-
-export const StartEndPoint = async ({ inFolderName, inToPath }) => {
-    return startFunc({
-        inCommandToSend: "StartEndPoint",
-        inFolderName, inToPath
-    });
-};
+export default load;
--- test/startEndPoint.js
+++ test/startEndPoint.js
@@ -1,19 +1,7 @@
-import getLatestVersion from "../bin/core/getLatestVersion.js";
-
-const commandToSend = "StartEndPoint";
-
-const load = async () => {
-    const v = getLatestVersion();
-
-    return import(`../bin/${v}/core/resolveCommand.js`);
-};
+import index from "../index.js";
 
 const startFunc = async () => {
-    const { default: run } = await load();
-
-    const fn = run(commandToSend);
-
-    fn({
+    index({
         folderName: "aaaaaaaaaaa",
         toPath: process.cwd(),
         showLog: false,

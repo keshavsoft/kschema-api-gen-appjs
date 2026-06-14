9c887b0d59cff47fe875b00f9c706f7f0cd382a8
keshavsoft <nknnkumar@yahoo.com>
2026-06-14 23:15:26 +0530

    to test

--- bin/v7/StartEndPoint/Helpers/steps/UpdateAppJs/index.js
+++ bin/v7/StartEndPoint/Helpers/steps/UpdateAppJs/index.js
@@ -1,12 +1,13 @@
 import fs from "fs";
 import checkLines from "./checkLines.json" with {type: "json"};
-import load from "express-fix-any-js";
+import load from "express-fix-app-js";
 
 const updateAppJs = ({ appJsPath, endpoint, showLog }) => {
     load({
         showLog,
         jsFilePath: appJsPath,
-        inCheckLines: checkLines
+        inCheckLines: checkLines,
+        inStartEndPoint: endpoint
     });
 
     return false;
